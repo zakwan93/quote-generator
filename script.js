b5bc8096481bbaef55e766c122c9e4527a76b1ce
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-06-15 10:52:52 -0400

    display data on page using dom and add tweet functionality

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQoutes = [];
 
+//Show Loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function newQuote(){
+    loading();
     const quote = apiQoutes[Math.floor(Math.random() * apiQoutes.length )];
-    console.log(quote);
+    // console.log(quote);
+    if(!quote.author){
+        authorText.textContent = 'Unknown'
+    }else{
+        authorText.textContent = quote.author;
+    }
+    //Check quote length to determine styling
+    if(quote.text.length > 100){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    //Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    complete();
 }
 
 
 
 async function getQotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
@@ -17,5 +52,14 @@ async function getQotes(){
         alert(error);
     }
 }
+
+// Tweet Qotes
+function tweetQuotes(){
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, '_blank')
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuotes);
 
 getQotes();
